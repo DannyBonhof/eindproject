f3ffd429e1ea4e72561db7ed04e3cdbd09892537
Dylan <Dylangreven2@outlook.com>
2025-05-19 13:48:43 +0200

    Logo no background inplaats van met background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,5 @@
 @section('content')
     <h1>Home</h1>
     <p>Hier komt de inhoud van de "Home" pagina.</p>
-    <img src="{{ asset('images/Logoeindproject.png') }}" alt="Logo" />
+    <img src="{{ asset('images/Logo_eindproject_nobg.png') }}" alt="Logo" />
 @endsection
