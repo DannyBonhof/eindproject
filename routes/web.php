80c3e20059e0bd803d1817a9825714e1eeb48f43
Dylan <Dylangreven2@outlook.com>
2025-05-26 13:23:56 +0200

    bietn

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/series', function () {
-    return view('series');
-})->name('series');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/overons', function () {
-    return view('overons');
-})->name('overons');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/sponsoren', function () {
-    return view('sponsoren');
-})->name('sponsoren');
+require __DIR__.'/auth.php';
