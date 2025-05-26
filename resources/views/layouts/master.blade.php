5238cbd1b369743bab882b4aa1bb7cae00dd97cc
Dylan <Dylangreven2@outlook.com>
2025-05-26 11:44:38 +0200

    Login dingen gefixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Standaard Homepagina</title>
+    <title>NMBD</title>
     <link rel="stylesheet" href="{{ asset('css/master.css') }}">
 </head>
 
