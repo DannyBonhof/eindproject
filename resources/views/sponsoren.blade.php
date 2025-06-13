e4d881aec1f818f4836e8bf0c1bf56200c0c43de
Dylan <Dylangreven2@outlook.com>
2025-06-13 11:14:31 +0200

    Advertenties + cigaroots

--- resources/views/sponsoren.blade.php
+++ resources/views/sponsoren.blade.php
@@ -3,7 +3,18 @@
 @section('title', 'Privacy')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/sponsoren.css') }}">
 
-<img id="AdPilsL" src="{{ asset('images/Bietn_Pils.png') }}" alt="Advertentie">
+<div id="AdCigaContainer">
+    <img id="AdCiga" src="{{ asset('images/CigarootsAd.png') }}" alt="Advertentie">
+    <div class="adCiga-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia unde blanditiis nobis ratione maxime non laborum aliquid nostrum animi cupiditate saepe optio aperiam expedita excepturi, quas officiis vel pariatur iste.</div>
+</div>
+
+<div id="AdPilsContainer">
+    <img id="AdPils" src="{{ asset('images/Bietn_Pils.png') }}" alt="Advertentie">
+    <div class="adpils-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia unde blanditiis nobis ratione maxime non laborum aliquid nostrum animi cupiditate saepe optio aperiam expedita excepturi, quas officiis vel pariatur iste.</div>
+</div>
+
+
 
 @endsection
